33ec7757c5a8dcaa808f41af6be91fae6ce91891
Tanishk Gupta <134400664+tanishk2705@users.noreply.github.com>
2024-07-06 13:50:07 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
                     </p>
 
                     <div className='flex items-center self-start mt-4 lg:self-center'>
-                    <Link href='/dummy.pdf' target={'_blank'}
+                    <Link href='/forplacement.pdf' target={'_blank'}
                      className='flex items-center text-light bg-dark p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base'
                      download={true}>
                       Resume
